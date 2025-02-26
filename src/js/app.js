f4d75ec0beeaab15b225b699a3d6ac31acca56ea
Ammar Hassan <ammar@ammarhassan.co.uk>
2025-02-26 17:00:21 +0000

    update to script

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,7 @@ const divSection = document.querySelector('#adivce');
 const heading = document.querySelector('.heading');
 const p = document.querySelector('.advice');
 const btn = document.querySelector('.shuffleBtn')
+const h1 = document.querySelector('h1')
 
 document.addEventListener("DOMContentLoaded", function(){
     findMeAdvice();
@@ -11,7 +12,7 @@ async function findMeAdvice(){
         const endpoint = await fetch('https://api.adviceslip.com/advice');
         const data = await endpoint.json()
             const {id, advice} = data.slip;
-            heading.innerHTML = `Advice # ${id}`
+            h1.innerHTML = `Advice # ${id}`
             p.innerText = advice
             p.classList.add("text-white")
     }catch(error){
