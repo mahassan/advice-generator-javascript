fda5f38437bc915b91d70287fa60431076f52036
Ammar Hassan <ammar@ammarhassan.co.uk>
2025-02-27 17:44:11 +0000

    mix changes

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,8 @@
 const divSection = document.querySelector('#adivce');
 const heading = document.querySelector('.heading');
 const p = document.querySelector('.advice');
-const btn = document.querySelector('.shuffleBtn')
-const h1 = document.querySelector('h1')
+const btn = document.querySelector('.shuffleBtn');
+const h1 = document.querySelector('h1');
 
 document.addEventListener("DOMContentLoaded", function(){
     findMeAdvice();
@@ -12,6 +12,7 @@ async function findMeAdvice(){
         const endpoint = await fetch('https://api.adviceslip.com/advice');
         const data = await endpoint.json()
             const {id, advice} = data.slip;
+
             h1.innerHTML = `Advice # ${id}`
             p.innerText = advice
             p.classList.add("text-white")
